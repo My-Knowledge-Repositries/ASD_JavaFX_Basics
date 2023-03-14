e8c34cc16c3b2df32cb61098f7698746e797ce15
oshadaera68 <mindartlk010@gmail.com>
2023-03-14 14:53:40 +0530

    image finished

---
+++ App11/src/MainForm5Controller.java
@@ -0,0 +1,3 @@
+public class MainForm5Controller {
+
+}
--- App7/src/MainForm1Controller.java
+++ App7/src/MainForm1Controller.java
@@ -2,20 +2,39 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainForm1Controller {
     public ComboBox<String> cmbLanguage;
+    ArrayList<String> myLangList = new ArrayList<>(
+            Arrays.asList("Java","Php","C#","Kotlin")
+    );
 
     public void initialize() {
+        System.out.println(myLangList);
         // listener
         cmbLanguage.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            System.out.println("new value : " +newValue);
-            System.out.println("old value : " +oldValue);
+            System.out.println("new value : " + newValue);
+            System.out.println("old value : " + oldValue);
         });
 
         loadData();
     }
 
     private void loadData() {
+        /*First Method*/
+        /*
+        cmbLanguage.getItems().add("Java");
+        cmbLanguage.getItems().add("Php");
+        cmbLanguage.getItems().add("C#");
+        cmbLanguage.getItems().add("Ruby");
+        */
+
+        /*Second Method*/
+        /*cmbLanguage.getItems().addAll("Java", "Php", "C#", "Ruby");*/
+
+        /*Third Method - using observable list*/
         ObservableList<String> list = FXCollections.observableArrayList();
         list.addAll("Java", "Php", "C#", "Ruby");
         cmbLanguage.setItems(list);
