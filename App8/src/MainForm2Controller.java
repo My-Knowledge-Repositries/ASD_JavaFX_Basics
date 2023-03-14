25a874ea24311918e14a12ef0bd096ce8c800ee5
oshadaera68 <mindartlk010@gmail.com>
2023-03-14 06:42:27 +0530

    added listener

--- App8/src/MainForm2Controller.java
+++ App8/src/MainForm2Controller.java
@@ -4,19 +4,26 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class MainForm2Controller {
-    public TextField txtInputLanguages;
+    public TextField txtInputLanguage;
     public ListView<String> lstLanguages;
+    public TextField txtSelectedLanguage;
+
+    public void initialize(){
+        lstLanguages.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            txtSelectedLanguage.setText(newValue);
+        });
+    }
 
     public void addDataOnAction(ActionEvent actionEvent) {
-        String value = txtInputLanguages.getText();
+        String value = txtInputLanguage.getText();
         if (isExists(value)) {
             Alert alert = new Alert(Alert.AlertType.WARNING, "Alerady exists");
             alert.show();
             return;
         }
-        lstLanguages.getItems().add(txtInputLanguages.getText());
+        lstLanguages.getItems().add(txtInputLanguage.getText());
         //txtInputLanguages.setText("");
-        txtInputLanguages.clear(); // new java fx code
+        txtInputLanguage.clear(); // new java fx code
     }
 
     private boolean isExists(String value) {
