a0e87a3eba2f963b41b391515645d036417ac465
oshadaera68 <mindartlk010@gmail.com>
2023-03-14 06:36:55 +0530

    list finished

--- App8/src/MainForm2Controller.java
+++ App8/src/MainForm2Controller.java
@@ -1,2 +1,28 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class MainForm2Controller {
+    public TextField txtInputLanguages;
+    public ListView<String> lstLanguages;
+
+    public void addDataOnAction(ActionEvent actionEvent) {
+        String value = txtInputLanguages.getText();
+        if (isExists(value)) {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Alerady exists");
+            alert.show();
+            return;
+        }
+        lstLanguages.getItems().add(txtInputLanguages.getText());
+        //txtInputLanguages.setText("");
+        txtInputLanguages.clear(); // new java fx code
+    }
+
+    private boolean isExists(String value) {
+        for (String tempData : lstLanguages.getItems()) {
+            if (tempData.equalsIgnoreCase(value)) return true;
+        }
+        return false;
+    }
 }
